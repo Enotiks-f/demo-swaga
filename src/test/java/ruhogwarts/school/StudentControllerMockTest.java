d189eb406a23b5ea07b51772c082ea01c75b65a9
Titov Ilya <titov1608@gmail.com>
2026-01-28 13:08:17 +0300

    test-Mock-faculty

--- src/test/java/ruhogwarts/school/StudentControllerMockTest.java
+++ src/test/java/ruhogwarts/school/StudentControllerMockTest.java
@@ -1,21 +1,17 @@
 package ruhogwarts.school;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 import org.springframework.web.multipart.MultipartFile;
 import ruhogwarts.school.controller.StudentController;
-import ruhogwarts.school.model.Avatar;
 import ruhogwarts.school.model.Faculty;
 import ruhogwarts.school.model.Student;
 import ruhogwarts.school.service.AvatarService;
@@ -26,7 +22,6 @@ import java.util.Optional;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.multipart;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -37,9 +32,6 @@ public class StudentControllerMockTest {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @Autowired
-    private ObjectMapper objectMapper;
 
     @MockitoBean
     private StudentService service;
